3f5467b591d7507e17aafea31ae0c44ca9ad858b
Edgar <edgarfrodri@gmail.com>
2023-10-08 12:35:32 +0200

    chore: mini-max-sum

---
+++ mini-max-sum/problem.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func miniMaxSum(arr []int32) {
+	var min int64 = int64(math.Pow(float64(10), float64(9)))
+	var max int64 = 1
+	var sum int64
+	for _, val := range arr {
+		val64 := int64(val)
+		sum += val64
+		if val64 > max {
+			max = val64
+		}
+		if val64 < min {
+			min = val64
+		}
+	}
+	fmt.Println(sum-max, sum-min)
+
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < 5; i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	miniMaxSum(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
